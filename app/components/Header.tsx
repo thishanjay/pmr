adf0b4c206ded58ddec62b1388e49d2e5a763064
thishanjay <thishanishere@gmail.com>
2026-04-07 20:01:09 +0530

    updated header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,8 +6,8 @@ const Header = () => {
     //  mt-10
     <div className="">
       {/* flex flex-col md:flex-row items-center md:justify-between ml-4 md:ml-10 gap-4 text-center md:text-left */}
-      <div className="flex">
-        <Image src="/uop_crest.png" alt="uop crest" width={80} height={80} className="md:w-[100px] md:h-[100px]"/>
+      <div className="flex flex-col md:flex-row items-center md:justify-between ml-4 md:ml-10 gap-4 text-center md:text-left">
+        <Image src="/uop_crest.png" alt="uop crest" width={200} height={200} className="md:w-[120px] md:h-[120px]"/>
         <div>
           <h1 className="text-2xl md:text-4xl font-bold">
             Peradeniya Management Review (PMR)
@@ -19,7 +19,7 @@ const Header = () => {
         
         {/* Added 'hidden md:block' to control visibility */}
         <div className="hidden md:block">
-          <Image src="/moon-stone.png" alt="uom logo" width={50} height={50} />
+          <Image src="/moon-stone.png" alt="uom logo" width={80} height={80} />
         </div>
       </div>
 
