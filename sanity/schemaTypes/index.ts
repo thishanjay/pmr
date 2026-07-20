88107972f4de585a8813c80489d4c320f2761bbc
thishanjay <thishanishere@gmail.com>
2026-07-21 00:27:45 +0530

    Customize sanity

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,19 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
+// Import your schema files
+import mostRead from './MostRead'
+import mostCited from './mostCited'
+import editorialBoard from './editorialBoard'
+import volume from './volume'
+import issue from './issue'
+
+// Export schema object
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [
+    mostRead,
+    mostCited,
+    editorialBoard,
+    volume,
+    issue,
+  ],
 }
--- sanity/schemaTypes/mostRead.ts
+++ sanity/schemaTypes/mostRead.ts
@@ -1,12 +1,12 @@
 import { defineType } from 'sanity';
 
 export default defineType({
-  name: 'mostRead',
-  title: 'Most Read',
-  type: 'document',
-  fields: [
-    { name: 'title', title: 'Title', type: 'string' },
-    { name: 'citationCount', title: 'Citation Count', type: 'number' },
-    { name: 'url', title: 'URL', type: 'url' }
-  ]
+    name: 'mostRead',
+    title: 'Most Read',
+    type: 'document',
+    fields: [
+        { name: 'title', title: 'Title', type: 'string' },
+        { name: 'citationCount', title: 'Citation Count', type: 'number' },
+        { name: 'url', title: 'URL', type: 'url' }
+    ]
 });
